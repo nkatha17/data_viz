b865ed8da90fb0c459937cb20d9a3eb15e913025
nkatha17 <119823184+nkatha17@users.noreply.github.com>
2022-12-04 19:44:54 -0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install streamlit')
+
+
+# In[2]:
+
+
+import streamlit as st
+import pandas as pd
+
+
+# In[3]:
+
+
+st.write("#Data Science Jobs Trend Analysis ")
+
+
+# In[ ]:
+
+
+
+
